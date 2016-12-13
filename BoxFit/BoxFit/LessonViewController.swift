f2c09d59c4d60e8e85180c9e1a807d1d11fa38d2
Lorsen <omarfroa@gmail.com>
2016-12-13 16:01:15 -0600

    learning view to lesson view working

---
+++ BoxFit/BoxFit/LessonViewController.swift
@@ -0,0 +1,37 @@
+//
+//  LessonViewController.swift
+//  BoxFit
+//
+//  Created by Omar Roa on 12/12/16.
+//  Copyright © 2016 Omar Roa. All rights reserved.
+//
+
+import UIKit
+
+class LessonViewController: UIViewController {
+
+    var activityToReceive: String = "not chosen"
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
