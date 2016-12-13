83945c2fca400c95ae6416c76660c2c3f4dd45b9
Lorsen <omarfroa@gmail.com>
2016-12-12 20:53:20 -0600

    Add Collection view to learning

---
+++ BoxFit/BoxFit/ImageCell.swift
@@ -0,0 +1,14 @@
+//
+//  ImageCell.swift
+//  BoxFit
+//
+//  Created by Omar Roa on 12/12/16.
+//  Copyright © 2016 Omar Roa. All rights reserved.
+//
+
+import UIKit
+
+class ImageCell: UICollectionViewCell {
+    
+    @IBOutlet weak var image: UIImageView!
+}
