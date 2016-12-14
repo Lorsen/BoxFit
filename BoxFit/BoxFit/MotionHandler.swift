28fee86356aa1c55b84f43070291583b8c87bbb8
Lorsen <omarfroa@gmail.com>
2016-12-14 05:59:49 -0600

    add jacob's motion controller and some ui changes

--- BoxFit/BoxFit/MotionHandler.swift
+++ BoxFit/BoxFit/MotionHandler.swift
@@ -5,16 +5,15 @@ import MotionKit
 // https://github.com/raywenderlich/swift-algorithm-club/tree/master/Ring%20Buffer
 // Thank you Matthijs Hollemans, whoever you are
 //
-
 public struct RingBuffer<T> {
     public var array: [T?]
     public var readIndex = 0
     fileprivate var writeIndex = 0
-
+    
     public init(count: Int) {
         array = [T?](repeating: nil, count: count)
     }
-
+    
     public mutating func write(_ element: T) -> Bool {
         if !isFull {
             array[writeIndex % array.count] = element
@@ -24,7 +23,7 @@ public struct RingBuffer<T> {
             return false
         }
     }
-
+    
     public mutating func read() -> T? {
         if !isEmpty {
             let element = array[readIndex % array.count]
@@ -34,19 +33,19 @@ public struct RingBuffer<T> {
             return nil
         }
     }
-
+    
     fileprivate var availableSpaceForReading: Int {
         return writeIndex - readIndex
     }
-
+    
     public var isEmpty: Bool {
         return availableSpaceForReading == 0
     }
-
+    
     fileprivate var availableSpaceForWriting: Int {
         return array.count - availableSpaceForReading
     }
-
+    
     public var isFull: Bool {
         return false
     }
@@ -57,12 +56,12 @@ class MotionHandler: NSObject {
     private let motionKit = MotionKit()
     public var accelBuffer = RingBuffer<(Double, Double, Double)>(count: 50)
     public var gyroBuffer = RingBuffer<(Double, Double, Double)>(count: 50)
-
+    
     var listenForEvent = false
     init(i: Double) {
         self.interval = i
     }
-
+    
     public func start() {
         self.motionKit.getAccelerometerValues(self.interval) {
             (x, y, z) in
@@ -79,14 +78,13 @@ class MotionHandler: NSObject {
         motionKit.stopGyroUpdates()
     }
     
-    public func getNextMotion(timeout: Double) -> [[Double]]? {
+    public func getNextMotion(timeout: Double) -> [String:[String:String]]? {
         let time = Date()
         
         self.listenForEvent = true
         var previousAccel = [(Double, Double, Double)](repeating: (Double(), Double(), Double()), count: 4)
         var previousGyro = [(Double, Double, Double)](repeating: (Double(), Double(), Double()), count: 4)
         let threshold = 1.0
-        
         
         while(Date().timeIntervalSince(time) < 1.0) {
             for i in 0...3 {
@@ -108,11 +106,13 @@ class MotionHandler: NSObject {
             }
         }
         return nil
-
+        
     }
     
-    public func getData() -> [[Double]] {
+    public func getData() -> [String:[String:String]] {
         var data = [[Double]](repeating:[Double](repeating:Double(), count:6), count:50)
+        var mean_array = [Double](repeating:Double(), count:6)
+        
         // populate data from ring buffers
         for i in 0...49 {
             let accelIndex = (self.accelBuffer.writeIndex - i) % 50
@@ -123,14 +123,47 @@ class MotionHandler: NSObject {
             data[i][3] = (self.gyroBuffer.array[gyroIndex]?.0)!
             data[i][4] = (self.gyroBuffer.array[gyroIndex]?.1)!
             data[i][5] = (self.gyroBuffer.array[gyroIndex]?.2)!
+            for j in 0...5 {
+                mean_array[j] += data[i][j]
+            }
         }
+        
         // process data by averaging with a sliding window
         for i in 1...48 {
             for j in 0...5 {
                 data[i][j] = (data[i-1][j] + data[i][j] + data[i+1][j]) / 3.0
             }
         }
-        return data
+        
+        // create mean array
+        for i in 0...5 {
+            mean_array[i] /= 50
+        }
+        
+        // create standard dev mean array
+        var standard_dev_mean_array = [Double](repeating:Double(), count:6)
+        for i in 0...49 {
+            for j in 0...5 {
+                standard_dev_mean_array[j] += mean_array[j] - data[i][j]
+            }
+        }
+        for i in 0...5 {
+            standard_dev_mean_array[i] /= 50
+        }
+        
+        data.append(mean_array)
+        data.append(standard_dev_mean_array)
+        
+        var string_dict = [String:[String:String]]()
+        // convert to string dictionary
+        for i in 0...51 {
+            var nested_dict = [String:String]()
+            for j in 0...5 {
+                nested_dict["\(i)-\(j)"] = "\(data[i][j])"
+            }
+            string_dict["\(i)"] = nested_dict
+        }
+        
+        return string_dict
     }
 }
-
