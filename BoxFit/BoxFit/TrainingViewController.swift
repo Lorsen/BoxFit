a8c801cb9a1a39907e827ebafac8db5920ab74de
Lorsen <omarfroa@gmail.com>
2016-12-14 03:12:50 -0600

    for the love of god, don't mess this up. please....

--- BoxFit/BoxFit/TrainingViewController.swift
+++ BoxFit/BoxFit/TrainingViewController.swift
@@ -8,10 +8,22 @@
 
 import UIKit
 
-class TrainingViewController: UIViewController {
+class TrainingViewController: UIViewController,  UICollectionViewDelegateFlowLayout, UICollectionViewDataSource {
 
     @IBOutlet weak var TrainingScroll: UIScrollView!
     @IBOutlet weak var RoutineScroll: UIScrollView!
+    
+    let trainingCollectionViewIdentifier = "TrainingCell"
+    let routineCollectionViewIdentifier = "RoutineCell"
+    
+    @IBOutlet weak var trainingCollectionView: UICollectionView!
+    @IBOutlet weak var routineCollectionView: UICollectionView!
+    
+    var trainingImageData: [String] = [String]()
+    var trainingImageCounter: Int = 0
+    
+    var routineImageData: [String] = [String]()
+    var routineImageCounter: Int = 0
     
     var defaults: UserDefaults!
     override func viewDidLoad() {
@@ -27,6 +39,10 @@ class TrainingViewController: UIViewController {
             defaults.set(0, forKey: "lifetimePunches")
             defaults.set(true, forKey: "returningUser")
         }
+        
+        trainingImageData = ["jab", "hook", "uppercut", "block"]
+        routineImageData = ["routine1", "routine2", "routine3"]
+
     }
 
     override func didReceiveMemoryWarning() {
@@ -34,15 +50,98 @@ class TrainingViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        
+        if collectionView == self.trainingCollectionView {
+            let trainingCell = collectionView.dequeueReusableCell(withReuseIdentifier: trainingCollectionViewIdentifier, for: indexPath as IndexPath) as! ImageCell
+            
+            trainingCell.backgroundColor = UIColor.red
+            var currImage:String = ""
+            currImage = self.trainingImageData[self.trainingImageCounter]
+            
+            if(self.trainingImageCounter == 0){
+                trainingCell.movement = "Jab"
+            }
+                
+            else if(self.trainingImageCounter == 1){
+                trainingCell.movement = "Hook"
+            }
+                
+            else if(self.trainingImageCounter == 2){
+                trainingCell.movement = "UpperCut"
+            }
+                
+            else if(self.trainingImageCounter == 3 ){
+                trainingCell.movement = "Block"
+            }
+            
+            self.trainingImageCounter += 1
+            
+            if self.trainingImageCounter >= self.trainingImageData.count {
+                self.trainingImageCounter = 0
+            }
+            
+            trainingCell.trainingImage.image = UIImage(named: currImage)
+            return trainingCell
+        }
+            
+        else{
+            let routineCell = collectionView.dequeueReusableCell(withReuseIdentifier: routineCollectionViewIdentifier, for: indexPath as IndexPath) as! ImageCell
+            
+            routineCell.backgroundColor = UIColor.red
+            var currImage:String = ""
+            currImage = self.routineImageData[self.routineImageCounter]
+            
+            if(self.routineImageCounter == 0){
+                routineCell.movement = "routine1"
+            }
+                
+            else if(self.routineImageCounter == 1){
+                routineCell.movement = "routine2"
+            }
+                
+            else if(self.trainingImageCounter == 2){
+                routineCell.movement = "routine3"
+            }
+            
+            self.routineImageCounter += 1
+            
+            if self.routineImageCounter >= self.routineImageData.count {
+                self.routineImageCounter = 0
+            }
+            
+            routineCell.routineImage.image = UIImage(named: currImage)
+            return routineCell
+            
+        }
     }
-    */
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        if collectionView == self.trainingCollectionView {
+            return 4
+        }
+        
+        return 3
+    }
+    
+    func numberOfSectionsInCollectionView(collectionView: UICollectionView) -> Int {
+        return 1
+    }
+    
+    
+    func collectionView(collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAtIndexPath indexPath: NSIndexPath) -> CGSize {
+        
+        return CGSize(width: 90, height: 90)
+    }
+    
+        override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+            let cell = sender as! ImageCell
+    
+            if(segue.identifier == "prepViewSegue") {
+                let nextViewController = (segue.destination as! PrepViewController)
+                nextViewController.type = cell.movement
+            }
+            
+        }
 
 }
